443b697d4c6e4e147e7b3beec5b765996f41ed61
lza_menace <lza_menace@protonmail.com>
2021-01-15 15:22:54 -0800

    starting work on getting critical errors posted to operators in mattermost chat

--- app/app.py
+++ app/app.py
@@ -10,11 +10,50 @@ app = create_app()
 
 @app.errorhandler(requests.exceptions.ConnectionError)
 def request_connection_error(e):
+    app.logger.error('error')
     return render_template('error.html'), 500
 
+dictConfig({
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+        },
+        "access": {
+            "format": "%(message)s",
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
+        "mattermost": {
+            "class": "app.factory.MattermostHandler",
+            "formatter": "default",
+            "level": "ERROR",
+        }
+    },
+    "loggers": {
+        "gunicorn.error": {
+            "handlers": ["console"] if app.debug else ["console", "mattermost"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "gunicorn.access": {
+            "handlers": ["console"] if app.debug else ["console"],
+            "level": "INFO",
+            "propagate": False,
+        }
+    },
+    "root": {
+        "level": "DEBUG" if app.debug else "INFO",
+        "handlers": ["console", "mattermost"] if app.debug else ["console", "mattermost"],
+    }
+})
 
 if __name__ == '__main__':
     app.run()
-    gunicorn_logger = logging.getLogger('gunicorn.error')
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
